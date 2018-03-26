c641436b3bf53efada30ea2716811c71e1d63668
Pati S <37409500+patistan10@users.noreply.github.com>
2018-03-26 13:45:29 -0700

    Update get_prob_dist_ver8_for20.m

--- get_prob_dist_ver8_for20.m
+++ get_prob_dist_ver8_for20.m
@@ -1,6 +1,6 @@
 function [RespMatrix,RespMatrixTraining,RespMatrixTesting,RespMatrixAllTrials,ProbDist,BinThresh,removedBlocks,selected_cells] = get_prob_dist_ver8_for20(ExpID,selected_cells,bins,folds,test_set)
 %UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%   SINGLE SESSION
 
 
     if isempty(ExpID)
